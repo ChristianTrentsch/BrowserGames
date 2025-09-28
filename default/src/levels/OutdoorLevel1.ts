d2ad9132f5eefbacdf0ffb16c528051216860de1
Christian Trentsch <ranger1985@live.de>
2025-09-29 00:51:19 +0200

    Outdoor Image überarbeitet - altes Image umbenannt - neues Image ohne feste Tree, Square, Water, Stone, House - dafür die neuen zerstörbaren Objekte integriert

--- default/src/levels/OutdoorLevel1.ts
+++ default/src/levels/OutdoorLevel1.ts
@@ -10,6 +10,12 @@ import { events, HERO_EXITS, CHANGE_LEVEL } from "../Events.js";
 import { CaveLevel1 } from "./CaveLevel1.js";
 import { SaveGame } from "../SaveGame.js";
 import { LevelId } from "../helpers/levelRegistry.js";
+import { Tree } from "./parts/Tree/Tree.js";
+import { Stone } from "./parts/Stone/Stone.js";
+import { Square } from "./parts/Square/Square.js";
+import { Water } from "./parts/Water/Water.js";
+import { Bush } from "./parts/Bush/Bush.js";
+import { House } from "./parts/House/House.js";
 
 export class OutdoorLevel1 extends Level {
 
@@ -52,6 +58,29 @@ export class OutdoorLevel1 extends Level {
     const exit = new Exit(gridCells(10), gridCells(3));
     this.addChild(exit);
 
+    this.addChild(new Bush(80, 48));
+
+    this.addChild(new Tree(64, 48));
+    this.addChild(new Tree(208, 64));
+    this.addChild(new Tree(224, 32));
+    
+    this.addChild(new Stone(192, 96));
+    this.addChild(new Stone(208, 96));
+    this.addChild(new Stone(224, 96));
+    
+    this.addChild(new Square(64, 64));
+    this.addChild(new Square(64, 80));
+    this.addChild(new Square(80, 80));
+    this.addChild(new Square(96, 64));
+    this.addChild(new Square(96, 80));
+    this.addChild(new Square(128, 48));
+    this.addChild(new Square(144, 48));
+
+    this.addChild(new Water(128, 80));
+    this.addChild(new Water(144, 80));
+
+    this.addChild(new House(224, 64));
+
     //** --- Create Hero and add to scene --- */
     // entweder geladene Position (Reload) oder die übergebene Startposition (Levelwechsel)
     this.heroStartPosition = SaveGame.loadHero(this.levelId, heroPosition ?? this.defaultHeroPosition);
@@ -61,7 +90,7 @@ export class OutdoorLevel1 extends Level {
     //** --- Prüfen, ob Item schon im Inventar ist, ansonsten erzeugen --- */
     if (!SaveGame.isInInventory("rodPurple")) {
       // erzeuge Item und lege position fest
-      const rodPurple = new Item(gridCells(7), gridCells(6), "rodPurple");
+      const rodPurple = new Item(gridCells(5), gridCells(4), "rodPurple");
       this.addChild(rodPurple);
     }
 
@@ -71,12 +100,12 @@ export class OutdoorLevel1 extends Level {
       left: this.generateWall(new Vector2(32, 32), new Vector2(32, 96), TILE_SIZE, "left"),
       top: this.generateWall(new Vector2(48, 16), new Vector2(240, 16), TILE_SIZE, "top"),
       bottom: this.generateWall(new Vector2(48, 112), new Vector2(240, 112), TILE_SIZE, "bottom"),
-      tree: ["64,48", "208,64", "224,32"],
-      stone: ["192,96", "208,96", "224,96"],
-      squares: ["64,64", "64,80", "80,64", "80,80", "128,48", "144,48"],
-      water: ["112,80", "128,80", "144,80", "160,80"],
-      house: ["224,64"],
-      nothing: ["240,32", "96,32", "80,32", "64,32", "48,32",],
+      // tree: ["64,48", "208,64", "224,32"],
+      // stone: ["192,96", "208,96", "224,96"],
+      // squares: ["64,64", "64,80", "80,64", "80,80", "128,48", "144,48"],
+      // water: ["112,80", "128,80", "144,80", "160,80"],
+      // house: ["224,64"],
+      // nothing: ["240,32", "96,32", "80,32", "64,32", "48,32",],
     };
 
     this.walls = new Set(Object.values(wallDefinitions).flat());
