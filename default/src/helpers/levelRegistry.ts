283b8a01a7b4dca712327ec3185f273cc860c09f
Christian Trentsch <ranger1985@live.de>
2025-10-17 02:36:09 +0200

    Start Level entfernt - war nur zum testen

--- default/src/helpers/levelRegistry.ts
+++ default/src/helpers/levelRegistry.ts
@@ -1,13 +1,11 @@
 import { OutdoorLevel1 } from "../levels/OutdoorLevel1.js";
 import { CaveLevel1 } from "../levels/CaveLevel1.js";
-import { StartLevel } from "../levels/StartLevel.js";
 // weitere Level hier importieren …
 
-export type LevelId = "OutdoorLevel1" | "CaveLevel1" | "StartLevel";
+export type LevelId = "OutdoorLevel1" | "CaveLevel1";
 
 export const levelRegistry: Record<string, typeof OutdoorLevel1 | typeof CaveLevel1> = {
     OutdoorLevel1,
     CaveLevel1,
-    StartLevel
     // "DungeonLevel2": DungeonLevel2, ...
 };
