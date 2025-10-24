9947c3a859dc903e04ca5915f4359e1a82479d60
Christian Trentsch <ranger1985@live.de>
2025-10-25 00:09:09 +0200

    class Deko - neue Funktion: makeWaterFrames für Animation von Wasser

--- default/src/levels/parts/Deko/Deko.ts
+++ default/src/levels/parts/Deko/Deko.ts
@@ -23,15 +23,15 @@ export class Deko extends GameObject {
 
     let frameIndexPattern = DEKO_IDLE;
     if (imageKey === "animWater") {
-      frameIndexPattern = Animations.makeWaterFrames(0)
+      frameIndexPattern = this.makeWaterFrames()
     }
 
     if (imageKey === "animLamp") {
-      frameIndexPattern = this.makeLampFrames(0)
+      frameIndexPattern = this.makeLampFrames()
     }
 
     if (imageKey === "animBushSmall") {
-      frameIndexPattern = this.makeStandardFrames(0)
+      frameIndexPattern = this.makeStandardFrames()
     }
 
     this.addChild(new Sprite({
@@ -45,7 +45,6 @@ export class Deko extends GameObject {
         dekoIdle: new FrameIndexPattern(frameIndexPattern)
       }),
     }));
-
   }
 
   ready() { }
@@ -70,6 +69,47 @@ export class Deko extends GameObject {
         {
           time: 450,
           frame: rootFrame,
+        },
+      ],
+    };
+  };
+
+  makeWaterFrames = (rootFrame = 0) => {
+
+    return {
+      duration: 2400,
+      frames: [
+        {
+          time: 0,
+          frame: rootFrame,
+        },
+        {
+          time: 300,
+          frame: rootFrame + 1,
+        },
+        {
+          time: 600,
+          frame: rootFrame + 2,
+        },
+        {
+          time: 900,
+          frame: rootFrame + 3,
+        },
+        {
+          time: 1200,
+          frame: rootFrame + 4,
+        },
+        {
+          time: 1500,
+          frame: rootFrame + 5,
+        },
+        {
+          time: 1800,
+          frame: rootFrame + 6,
+        },
+        {
+          time: 2100,
+          frame: rootFrame + 7,
         },
       ],
     };
