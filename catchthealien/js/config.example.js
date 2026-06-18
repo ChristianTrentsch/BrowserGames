7a0668fef7342ca9804897aea53de34d689e071e
Christian Trentsch <ranger1985@live.de>
2026-06-19 01:52:09 +0200

    Supabase - Url und Key ind seperate config.js ausgelagert - config.js im Html einbinden - config.example.js als Vorlage erstellt - gitignore erweitert

---
+++ catchthealien/js/config.example.js
@@ -0,0 +1,4 @@
+// ⚠️ Vor der Verwendung anpassen:
+// Trage hier die URL und den Anon-Key deines Supabase-Projekts ein.
+const SUPABASE_URL = "https://DEIN-PROJEKT.supabase.co";
+const SUPABASE_KEY = "DEIN-ANON-KEY";
